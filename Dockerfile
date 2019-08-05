f41323c3e77fb22af255962efe469d458b2e3bfa
RuthNjeri <ruthnwaiganjo@gmail.com>
2019-08-05 16:32:47 +0300

    add pg cron extension

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ LABEL Maintainer="Ruth Waiganjo <ruthnwaiganjo@gmail.com>"
 RUN apt-get update \
         && apt-get install -y postgresql-11-postgis-2.5 \
         && apt-get install postgresql-11-postgis-2.5-scripts \
-        && apt-get install -y postgresql-11-hll
+        && apt-get install -y postgresql-11-hll \
+        && apt-get -y install postgresql-11-cron
+
 
 RUN mkdir -p /docker-entrypoint-initdb.docker
 COPY ./dbextensions.sh /docker-entrypoint-initdb.d/dbextensions.sh
--- dbextensions.sh
+++ dbextensions.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
+# Set up pg_cron
+echo "Setting up pg_cron..."
+cat <<EOT >> ${PGDATA}/postgresql.conf
+shared_preload_libraries='pg_cron'
+cron.database_name='${POSTGRES_DB:-postgres}'
+EOT
 
+# Restart postgres after pg_cron setup
+echo "Restarting Postgres..."
+pg_ctl restart
+
+# Create Extensions
 echo "Creating extensions in $POSTGRES_DB as user $POSTGRES_USER"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
@@ -9,6 +20,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE EXTENSION IF NOT EXISTS hll;
     CREATE EXTENSION IF NOT EXISTS btree_gist;
     CREATE EXTENSION IF NOT EXISTS pgcrypto;
+    CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 EOSQL
 
